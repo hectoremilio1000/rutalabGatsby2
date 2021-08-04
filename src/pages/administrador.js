3ca69eb20a17d0384442888bb3c8e5c19ba6aa94
hectoremilio1000 <hectoremilio1000@gmail.com>
2021-08-04 08:05:27 -0500

    cambios navbar aws-config 2

--- src/pages/administrador.js
+++ src/pages/administrador.js
@@ -2,7 +2,7 @@ import * as React from "react";
 
 // Amplify Configuration
 
-import { AmplifyAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+// import { AmplifyAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 import { Link } from "gatsby";
 
 export default function Administrador() {
@@ -11,179 +11,177 @@ export default function Administrador() {
       <Link to="/" class="navbar-brand col-md-3 col-lg-2 mr-0 px-3">
         Home
       </Link>
-      <AmplifyAuthenticator>
-        <div>
-          <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow p-1">
-            <Link to="/" class="navbar-brand col-md-3 col-lg-2 mr-0 px-3">
-              Administrador
-            </Link>
-            <button
-              class="navbar-toggler position-absolute d-md-none collapsed"
-              type="button"
-              data-toggle="collapse"
-              data-target="#sidebarMenu"
-              aria-controls="sidebarMenu"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
+      {/* <AmplifyAuthenticator> */}
+      <div>
+        <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow p-1">
+          <Link to="/" class="navbar-brand col-md-3 col-lg-2 mr-0 px-3">
+            Administrador
+          </Link>
+          <button
+            class="navbar-toggler position-absolute d-md-none collapsed"
+            type="button"
+            data-toggle="collapse"
+            data-target="#sidebarMenu"
+            aria-controls="sidebarMenu"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <ul class="navbar-nav px-3 mt-5 mt-sm-0">
+            <li class="nav-item text-nowrap">{/* <AmplifySignOut /> */}</li>
+          </ul>
+        </nav>
+
+        <div class="container-fluid">
+          <div class="row">
+            <nav
+              id="sidebarMenu"
+              class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
             >
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <ul class="navbar-nav px-3 mt-5 mt-sm-0">
-              <li class="nav-item text-nowrap">
-                <AmplifySignOut />
-              </li>
-            </ul>
-          </nav>
+              <div class="sidebar-sticky pt-3">
+                <ul class="nav flex-column">
+                  <li class="nav-item">
+                    <Link
+                      to="/"
+                      class="navbar-brand col-md-3 col-lg-2 mr-0 px-3"
+                    >
+                      Administrador
+                    </Link>
+                  </li>
+                  <li class="nav-item">
+                    <p>
+                      <Link to="/ordenesAsignar">Órdenes por asignar</Link>!
+                    </p>
+                  </li>
+                  <li class="nav-item">
+                    <Link
+                      to="/"
+                      class="navbar-brand col-md-3 col-lg-2 mr-0 px-3"
+                    >
+                      Administrador
+                    </Link>
+                  </li>
+                  <li class="nav-item">
+                    <Link
+                      to="/"
+                      class="navbar-brand col-md-3 col-lg-2 mr-0 px-3"
+                    >
+                      Administrador
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </nav>
 
-          <div class="container-fluid">
-            <div class="row">
-              <nav
-                id="sidebarMenu"
-                class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
-              >
-                <div class="sidebar-sticky pt-3">
-                  <ul class="nav flex-column">
-                    <li class="nav-item">
-                      <Link
-                        to="/"
-                        class="navbar-brand col-md-3 col-lg-2 mr-0 px-3"
-                      >
-                        Administrador
-                      </Link>
-                    </li>
-                    <li class="nav-item">
-                      <p>
-                        <Link to="/ordenesAsignar">Órdenes por asignar</Link>!
-                      </p>
-                    </li>
-                    <li class="nav-item">
-                      <Link
-                        to="/"
-                        class="navbar-brand col-md-3 col-lg-2 mr-0 px-3"
-                      >
-                        Administrador
-                      </Link>
-                    </li>
-                    <li class="nav-item">
-                      <Link
-                        to="/"
-                        class="navbar-brand col-md-3 col-lg-2 mr-0 px-3"
-                      >
-                        Administrador
-                      </Link>
-                    </li>
-                  </ul>
-                </div>
-              </nav>
-
-              <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
-                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                  <h1 class="h2">Dashboard</h1>
-                  <div class="btn-toolbar mb-2 mb-md-0">
-                    <div class="btn-group mr-2">
-                      <button
-                        type="button"
-                        class="btn btn-sm btn-outline-secondary"
-                      >
-                        Share
-                      </button>
-                      <button
-                        type="button"
-                        class="btn btn-sm btn-outline-secondary"
-                      >
-                        Export
-                      </button>
-                    </div>
+            <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+              <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                <h1 class="h2">Dashboard</h1>
+                <div class="btn-toolbar mb-2 mb-md-0">
+                  <div class="btn-group mr-2">
                     <button
                       type="button"
-                      class="btn btn-sm btn-outline-secondary dropdown-toggle"
+                      class="btn btn-sm btn-outline-secondary"
                     >
-                      <span data-feather="calendar"></span>
-                      This week
+                      Share
+                    </button>
+                    <button
+                      type="button"
+                      class="btn btn-sm btn-outline-secondary"
+                    >
+                      Export
                     </button>
                   </div>
+                  <button
+                    type="button"
+                    class="btn btn-sm btn-outline-secondary dropdown-toggle"
+                  >
+                    <span data-feather="calendar"></span>
+                    This week
+                  </button>
                 </div>
+              </div>
 
-                <h2>Órdenes</h2>
-                <div class="table-responsive">
-                  <table class="table table-striped table-sm">
-                    <thead>
-                      <tr>
-                        <th># orden</th>
-                        <th>Hora pedido</th>
-                        <th>Hora entrega</th>
-                        <th>Servicio</th>
-                        <th>Cliente</th>
-                        <th>Dirección</th>
-                        <th>Asignado</th>
-                        <th>Resultados</th>
-                        <th>Informe</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>0001</td>
-                        <td>9:20</td>
-                        <td>13:20</td>
-                        <td>prueba embarazo</td>
-                        <td>Juan Pérez</td>
-                        <td>Independencia #323</td>
-                        <td>Juan</td>
-                        <td>PDF</td>
-                        <td></td>
-                      </tr>
-                      <tr>
-                        <td>0002</td>
-                        <td>9:20</td>
-                        <td>13:20</td>
-                        <td>prueba embarazo</td>
-                        <td>Juan Pérez</td>
-                        <td>Independencia #323</td>
-                        <td>Juan</td>
-                        <td>PDF</td>
-                        <td></td>
-                      </tr>
-                      <tr>
-                        <td>0003</td>
-                        <td>9:20</td>
-                        <td>13:20</td>
-                        <td>prueba embarazo</td>
-                        <td>Juan Pérez</td>
-                        <td>Independencia #323</td>
-                        <td>Juan</td>
-                        <td>PDF</td>
-                        <td></td>
-                      </tr>
-                      <tr>
-                        <td>0004</td>
-                        <td>9:20</td>
-                        <td>13:20</td>
-                        <td>prueba embarazo</td>
-                        <td>Juan Pérez</td>
-                        <td>Independencia #323</td>
-                        <td>Juan</td>
-                        <td>PDF</td>
-                        <td></td>
-                      </tr>
-                      <tr>
-                        <td>0005</td>
-                        <td>9:20</td>
-                        <td>13:20</td>
-                        <td>prueba embarazo</td>
-                        <td>Juan Pérez</td>
-                        <td>Independencia #323</td>
-                        <td>Juan</td>
-                        <td>PDF</td>
-                        <td></td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-              </main>
-            </div>
+              <h2>Órdenes</h2>
+              <div class="table-responsive">
+                <table class="table table-striped table-sm">
+                  <thead>
+                    <tr>
+                      <th># orden</th>
+                      <th>Hora pedido</th>
+                      <th>Hora entrega</th>
+                      <th>Servicio</th>
+                      <th>Cliente</th>
+                      <th>Dirección</th>
+                      <th>Asignado</th>
+                      <th>Resultados</th>
+                      <th>Informe</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>0001</td>
+                      <td>9:20</td>
+                      <td>13:20</td>
+                      <td>prueba embarazo</td>
+                      <td>Juan Pérez</td>
+                      <td>Independencia #323</td>
+                      <td>Juan</td>
+                      <td>PDF</td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td>0002</td>
+                      <td>9:20</td>
+                      <td>13:20</td>
+                      <td>prueba embarazo</td>
+                      <td>Juan Pérez</td>
+                      <td>Independencia #323</td>
+                      <td>Juan</td>
+                      <td>PDF</td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td>0003</td>
+                      <td>9:20</td>
+                      <td>13:20</td>
+                      <td>prueba embarazo</td>
+                      <td>Juan Pérez</td>
+                      <td>Independencia #323</td>
+                      <td>Juan</td>
+                      <td>PDF</td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td>0004</td>
+                      <td>9:20</td>
+                      <td>13:20</td>
+                      <td>prueba embarazo</td>
+                      <td>Juan Pérez</td>
+                      <td>Independencia #323</td>
+                      <td>Juan</td>
+                      <td>PDF</td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td>0005</td>
+                      <td>9:20</td>
+                      <td>13:20</td>
+                      <td>prueba embarazo</td>
+                      <td>Juan Pérez</td>
+                      <td>Independencia #323</td>
+                      <td>Juan</td>
+                      <td>PDF</td>
+                      <td></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </main>
           </div>
         </div>
-      </AmplifyAuthenticator>
+      </div>
+      {/* </AmplifyAuthenticator> */}
     </div>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import * as React from "react";
-import Amplify from "aws-amplify";
-import awsconfig from "../aws-exports";
+// import Amplify from "aws-amplify";
+// import awsconfig from "../aws-exports";
 import NavBarComponent from "../templates/NavBarComponent";
 
-Amplify.configure(awsconfig);
+// Amplify.configure(awsconfig);
 
 const IndexPage = () => {
   return (
