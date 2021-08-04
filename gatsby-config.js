339dc4dba55654f527e4bd61e53440cbe9862805
hectoremilio1000 <hectoremilio1000@gmail.com>
2021-08-04 07:25:27 -0500

    nav bar installed

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,15 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "rutalabwebapp",
   },
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-theme-ui`,
+      options: {
+        prismPreset: "night-owl",
+        preset: "@theme-ui/preset-funk",
+      },
+    },
+  ],
 };
