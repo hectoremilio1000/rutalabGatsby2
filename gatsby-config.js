54e86891cec4e18aeb9c1b92f08cd7ac50101344
hectoremilio1000 <hectoremilio1000@gmail.com>
2021-10-03 20:30:17 -0500

    icon square

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,22 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://www.rutalab.com",
     title: "rutalabwebapp",
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `RUTALAB`,
+        short_name: `RUTALAB`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/assets/logo/logo_rutalab_chico.png`,
+      },
+    },
+
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     {
