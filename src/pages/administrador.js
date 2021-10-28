471e7b86b06140c9b01c10bd7c62eb2b4cc56a79
hectoremilio1000 <hectoremilio1000@gmail.com>
2021-10-27 21:41:26 -0500

    reestart nuevo branch

--- src/pages/administrador.js
+++ src/pages/administrador.js
@@ -1,119 +1,126 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 
-//authenticator
-import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
-import { Auth } from "@aws-amplify/auth";
-import { API, graphqlOperation } from "aws-amplify";
+// //authenticator
+// import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+// import { Auth } from "@aws-amplify/auth";
+// import { API, graphqlOperation } from "aws-amplify";
 
-//importar queries
-import { getUser } from "../graphql/queries";
-import { createUser } from "../graphql/mutations";
+// //importar queries
+// import { getUser } from "../graphql/queries";
+// import { createUser } from "../graphql/mutations";
+
+// const Administrador = () => {
+//   const [nombre, setNombre] = useState("");
+//   const [edad, setEdad] = useState("");
+//   const [direccion, setDireccion] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [telefono, setTelefono] = useState("");
+
+//   const onSubmit = event => {
+//     event.preventDefault();
+//     console.log("datos enviados");
+//   };
+
+//   const saveUserDB = async user => {
+//     console.log(user);
+//     await API.graphql(graphqlOperation(createUser, { input: user }));
+//   };
+
+//   useEffect(() => {
+//     const updateUser = async () => {
+//       //obtener la info del usuario autenticado
+//       const userInfo = await Auth.currentAuthenticatedUser({
+//         bypassCache: true,
+//       });
+//       console.log(userInfo);
+//       //checar si está en la base de datos de dynamodb
+//       if (userInfo) {
+//         const userData = await API.graphql(
+//           graphqlOperation(getUser, { id: userInfo.attributes.sub })
+//         );
+//         console.log(userData);
+//         if (!userData.data.getUser) {
+//           const user = {
+//             nameuser: userInfo.attributes.email,
+//             emailuser: userInfo.attributes.email,
+//           };
+//           await saveUserDB(user);
+//         }
+//         console.log("user already exist");
+//       }
+//     };
+
+//     updateUser();
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Formulario para PDF</h1>
+//       <form className="row" onSubmit={onSubmit}>
+//         <div className="col-md-6">
+//           <h4 className="display-6">Nombre completo</h4>
+//           <input
+//             type="text"
+//             placeholder="Nombre paciente"
+//             className="form-control"
+//             onChange={e => setNombre(e.target.value)}
+//             value={nombre}
+//           />
+//         </div>
+//         <div className="col-md-6">
+//           <h4 className="display-6">Edad</h4>
+//           <input
+//             type="number"
+//             placeholder="Escribe la edad del paciente"
+//             className="form-control"
+//             onChange={e => setEdad(e.target.value)}
+//             value={edad}
+//           />
+//         </div>
+//         <div className="col-md-6">
+//           <h4 className="display-6">Dirección</h4>
+//           <input
+//             type="textarea"
+//             placeholder="Escribe la dirección del paciente"
+//             className="form-control"
+//             onChange={e => setDireccion(e.target.value)}
+//             value={direccion}
+//           />
+//         </div>
+//         <div className="col-md-6">
+//           <h4 className="display-6">Email</h4>
+//           <input
+//             type="email"
+//             placeholder="Escribe el email del paciente"
+//             className="form-control"
+//             onChange={e => setEmail(e.target.value)}
+//             value={email}
+//           />
+//         </div>
+//         <div className="col-md-6">
+//           <h4 className="display-6">Teléfono móvil</h4>
+//           <input
+//             type="number"
+//             placeholder="Escribe el teléfono móvil del paciente"
+//             className="form-control"
+//             onChange={e => setTelefono(e.target.value)}
+//             value={telefono}
+//           />
+//         </div>
+//         <button type="submit" className="btn btn-primary">
+//           Enviar
+//         </button>
+//       </form>
+//       <AmplifySignOut />
+//     </div>
+//   );
+// };
+
+// export default withAuthenticator(Administrador);
+import React from "react";
 
 const Administrador = () => {
-  const [nombre, setNombre] = useState("");
-  const [edad, setEdad] = useState("");
-  const [direccion, setDireccion] = useState("");
-  const [email, setEmail] = useState("");
-  const [telefono, setTelefono] = useState("");
-
-  const onSubmit = event => {
-    event.preventDefault();
-    console.log("datos enviados");
-  };
-
-  const saveUserDB = async user => {
-    console.log(user);
-    await API.graphql(graphqlOperation(createUser, { input: user }));
-  };
-
-  useEffect(() => {
-    const updateUser = async () => {
-      //obtener la info del usuario autenticado
-      const userInfo = await Auth.currentAuthenticatedUser({
-        bypassCache: true,
-      });
-      console.log(userInfo);
-      //checar si está en la base de datos de dynamodb
-      if (userInfo) {
-        const userData = await API.graphql(
-          graphqlOperation(getUser, { id: userInfo.attributes.sub })
-        );
-        console.log(userData);
-        if (!userData.data.getUser) {
-          const user = {
-            nameuser: userInfo.attributes.email,
-            emailuser: userInfo.attributes.email,
-          };
-          await saveUserDB(user);
-        }
-        console.log("user already exist");
-      }
-    };
-
-    updateUser();
-  }, []);
-
-  return (
-    <div>
-      <h1>Formulario para PDF</h1>
-      <form className="row" onSubmit={onSubmit}>
-        <div className="col-md-6">
-          <h4 className="display-6">Nombre completo</h4>
-          <input
-            type="text"
-            placeholder="Nombre paciente"
-            className="form-control"
-            onChange={e => setNombre(e.target.value)}
-            value={nombre}
-          />
-        </div>
-        <div className="col-md-6">
-          <h4 className="display-6">Edad</h4>
-          <input
-            type="number"
-            placeholder="Escribe la edad del paciente"
-            className="form-control"
-            onChange={e => setEdad(e.target.value)}
-            value={edad}
-          />
-        </div>
-        <div className="col-md-6">
-          <h4 className="display-6">Dirección</h4>
-          <input
-            type="textarea"
-            placeholder="Escribe la dirección del paciente"
-            className="form-control"
-            onChange={e => setDireccion(e.target.value)}
-            value={direccion}
-          />
-        </div>
-        <div className="col-md-6">
-          <h4 className="display-6">Email</h4>
-          <input
-            type="email"
-            placeholder="Escribe el email del paciente"
-            className="form-control"
-            onChange={e => setEmail(e.target.value)}
-            value={email}
-          />
-        </div>
-        <div className="col-md-6">
-          <h4 className="display-6">Teléfono móvil</h4>
-          <input
-            type="number"
-            placeholder="Escribe el teléfono móvil del paciente"
-            className="form-control"
-            onChange={e => setTelefono(e.target.value)}
-            value={telefono}
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Enviar
-        </button>
-      </form>
-      <AmplifySignOut />
-    </div>
-  );
+  return <h1>hola administrador</h1>;
 };
 
-export default withAuthenticator(Administrador);
+export default Administrador;
--- src/pages/clientes.js
+++ src/pages/clientes.js
@@ -1,99 +1,106 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "gatsby";
+// import React, { useEffect, useState } from "react";
+// import { Link } from "gatsby";
 
-/**insertar amplify auth */
-import { AmplifyAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
-import Auth from "@aws-amplify/auth";
+// /**insertar amplify auth */
+// import { AmplifyAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+// import Auth from "@aws-amplify/auth";
 
-/** import auth api */
-import { graphqlOperation, API } from "aws-amplify";
-import { getUser } from "../graphql/queries";
-import { createUser } from "../graphql/mutations";
+// /** import auth api */
+// import { graphqlOperation, API } from "aws-amplify";
+// import { getUser } from "../graphql/queries";
+// import { createUser } from "../graphql/mutations";
 
-const cliente = {
-  name: "Jampier Smith Vasquez Mija",
-  edad: "20",
-  direccion: "Calle Coronel Vidal 220 A",
-  email: "jampierv127@gmail.com",
-  telefono: "+51 915096462",
-};
+// const cliente = {
+//   name: "Jampier Smith Vasquez Mija",
+//   edad: "20",
+//   direccion: "Calle Coronel Vidal 220 A",
+//   email: "jampierv127@gmail.com",
+//   telefono: "+51 915096462",
+// };
 
-// markup
+// // markup
+// const Clientes = () => {
+//   //aquí vamos a fetch user
+//   const [user, setUser] = useState(null);
+
+//   console.log(user.username);
+
+//   useEffect(() => {
+//     const fetchUser = async () => {
+//       const userInfo = await Auth.currentAuthenticatedUser({
+//         bypassCache: true,
+//       });
+//       if (!userInfo) {
+//         return;
+//       }
+//       try {
+//         const userData = await API.graphql(
+//           graphqlOperation(getUser, { id: userInfo.attributes.sub })
+//         );
+
+//         if (userData) {
+//           setUser(userData.data.getUser);
+//         }
+//       } catch (e) {
+//         console.log(e);
+//       }
+//     };
+
+//     fetchUser();
+//   }, []);
+
+//   const saveuserDB = async user => {
+//     console.log(user);
+//     await API.graphql(graphqlOperation(createUser, { input: user }));
+//   };
+
+//   // aquí vamos a checar quién es la persona conectada
+//   useEffect(() => {
+//     const updateUser = async () => {
+//       const userInfo = await Auth.currentAuthenticatedUser({
+//         bypassCache: true,
+//       }).catch(err => console.log(err));
+//       console.log(userInfo);
+//       if (userInfo) {
+//         const userData = await API.graphql(
+//           graphqlOperation(getUser, { id: userInfo.attributes.sub })
+//         );
+//         console.log(userData);
+//         if (!userData.data.getUser) {
+//           const user = {
+//             id: userInfo.attributes.sub,
+//             username: userInfo.attributes.email,
+//           };
+//           saveuserDB(user);
+//         }
+//         console.log("user already exists");
+//       }
+//     };
+//     updateUser();
+//   }, []);
+
+//   return (
+//     <>
+//       <Link to="/" class="navbar-brand col-md-3 col-lg-2 mr-0 px-3">
+//         Home
+//       </Link>
+//       <AmplifyAuthenticator>
+//         <h1>Hola mi nombre es {user.username}</h1>
+//         <AmplifySignOut>
+//           <button>
+//             <p>Salir de aquí pulsa click</p>
+//           </button>
+//         </AmplifySignOut>
+//       </AmplifyAuthenticator>
+//     </>
+//   );
+// };
+
+// export default Clientes;
+
+import React from "react";
 const Clientes = () => {
-  //aquí vamos a fetch user
-  const [user, setUser] = useState(null);
-
-  console.log(user.username);
-
-  useEffect(() => {
-    const fetchUser = async () => {
-      const userInfo = await Auth.currentAuthenticatedUser({
-        bypassCache: true,
-      });
-      if (!userInfo) {
-        return;
-      }
-      try {
-        const userData = await API.graphql(
-          graphqlOperation(getUser, { id: userInfo.attributes.sub })
-        );
-
-        if (userData) {
-          setUser(userData.data.getUser);
-        }
-      } catch (e) {
-        console.log(e);
-      }
-    };
-
-    fetchUser();
-  }, []);
-
-  const saveuserDB = async user => {
-    console.log(user);
-    await API.graphql(graphqlOperation(createUser, { input: user }));
-  };
-
-  // aquí vamos a checar quién es la persona conectada
-  useEffect(() => {
-    const updateUser = async () => {
-      const userInfo = await Auth.currentAuthenticatedUser({
-        bypassCache: true,
-      }).catch(err => console.log(err));
-      console.log(userInfo);
-      if (userInfo) {
-        const userData = await API.graphql(
-          graphqlOperation(getUser, { id: userInfo.attributes.sub })
-        );
-        console.log(userData);
-        if (!userData.data.getUser) {
-          const user = {
-            id: userInfo.attributes.sub,
-            username: userInfo.attributes.email,
-          };
-          saveuserDB(user);
-        }
-        console.log("user already exists");
-      }
-    };
-    updateUser();
-  }, []);
-
-  return (
-    <>
-      <Link to="/" class="navbar-brand col-md-3 col-lg-2 mr-0 px-3">
-        Home
-      </Link>
-      <AmplifyAuthenticator>
-        <h1>Hola mi nombre es {user.username}</h1>
-        <AmplifySignOut>
-          <button>
-            <p>Salir de aquí pulsa click</p>
-          </button>
-        </AmplifySignOut>
-      </AmplifyAuthenticator>
-    </>
-  );
+  return <h1>hola clientes</h1>;
 };
 
 export default Clientes;
