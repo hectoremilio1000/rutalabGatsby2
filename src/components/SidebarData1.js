557f28630c2f27d66ad15a3b59f908ec62bdb4aa
hectoremilio1000 <hectoremilio1000@gmail.com>
2021-10-31 14:21:41 -0600

    reescribir iniciarsesion

---
+++ src/components/SidebarData1.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { FaAngleDown } from "react-icons/fa";
+import { FaSortAmountUpAlt } from "react-icons/fa";
+import { FaCogs } from "react-icons/fa";
+import { FaUserInjured } from "react-icons/fa";
+import { IoPieChartSharp } from "react-icons/io5";
+import { FaVial } from "react-icons/fa";
+import { FaAngleUp } from "react-icons/fa";
+import { FaRegChartBar } from "react-icons/fa";
+
+export const SidebarData = [
+  {
+    title: "OverView",
+    path: "/administrador",
+    icon: <IoPieChartSharp />,
+  },
+  {
+    title: "Pacientes",
+    path: "/registerClients",
+    icon: <FaUserInjured />,
+  },
+  {
+    title: "Test / Orden",
+    path: "/registerOrden",
+    icon: <FaVial />,
+  },
+  {
+    title: "Levantar Orden",
+    path: "/levantar_orden",
+    icon: <FaSortAmountUpAlt />,
+    iconClosed: <FaAngleDown />,
+    iconOpened: <FaAngleUp />,
+  },
+  {
+    title: "Reportes",
+    path: "/reportes",
+    icon: <FaRegChartBar />,
+    iconClosed: <FaAngleDown />,
+    iconOpened: <FaAngleUp />,
+  },
+  {
+    title: "Configuracion",
+    path: "/settings",
+    icon: <FaCogs />,
+    iconClosed: <FaAngleDown />,
+    iconOpened: <FaAngleUp />,
+  },
+];
--- src/components/navDashboard.js
+++ src/components/navDashboard.js
@@ -3,21 +3,17 @@ import { FaBell } from "react-icons/fa";
 import { FaAlignJustify } from "react-icons/fa";
 import "../styles/navDashboard.css";
 
-//amplify
-const avatar_user =
-  "https://image.freepik.com/foto-gratis/hombre-joven-sonriente-playa_23-2147645587.jpg";
-
 const NavDashboard = props => {
   const { navSidebar, show_sidebar } = props;
-
+  const avatar_user =
+    "https://image.freepik.com/foto-gratis/hombre-joven-sonriente-playa_23-2147645587.jpg";
+  const handler = () => {
+    show_sidebar();
+  };
   return (
     <>
       <nav className={`navbar-dashboard ${!navSidebar ? "close_sidebar" : ""}`}>
-        <div
-          className="drop-down-menu"
-          onClick={show_sidebar}
-          role="presentation"
-        >
+        <div role="presentation" className="drop-down-menu" onClick={handler}>
           <FaAlignJustify />
         </div>
         <div className="user-login">
