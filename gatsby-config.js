e2b02fff420b4e4c1c3d998bc039d0369a64b493
hectoremilio1000 <hectoremilio1000@gmail.com>
2021-11-09 03:40:31 -0600

    agregar whatsapp

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,5 +26,30 @@ module.exports = {
         preset: "@theme-ui/preset-funk",
       },
     },
+    {
+      resolve: "gatsby-plugin-whatshelp",
+      options: {
+        // facebook: "xxx", // Facebook page ID
+        whatsapp: "+52 1 951 102 8474", // WhatsApp number
+        // email: "xxx@xxx.com", // Email
+        // call: "+52 1 951 102 8474", // Call phone number
+        company_logo_url:
+          "https://imagenesrutalab.s3.amazonaws.com/sanmateo/logo/logo_sanmateo_final.jpg", // URL of company logo (png, jpg, gif)
+        greeting_message: "Hello", // Text of greeting message
+        call_to_action: "Call to Action", // Call to action
+        button_color: "#b02030", // Color of button
+        position: "right", // Position may be 'right' or 'left'
+        order: "facebook,whatsapp,call,email", // Order of buttons
+        ga: true, // Google Analytics enabled
+        branding: false, // Show branding string
+        mobile: true, // Mobile version enabled
+        desktop: true, // Desktop version enabled
+        greeting: true, // Greeting message enabled
+        shift_vertical: 0, // Vertical position, px
+        shift_horizontal: 0, // Horizontal position, px
+        domain: "https://nuevo.d8s0msgulza43.amplifyapp.com/", // site domain
+        // key: "xxx", // pro-widget key
+      },
+    },
   ],
 };
