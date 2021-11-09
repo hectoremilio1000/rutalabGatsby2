5a3b73d4baf6f5a0ebb21d9c32243b9ffcc1fb67
hectoremilio1000 <hectoremilio1000@gmail.com>
2021-11-09 04:33:39 -0600

    agregar button whats

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,5 +26,30 @@ module.exports = {
         preset: "@theme-ui/preset-funk",
       },
     },
+    {
+      resolve: "gatsby-plugin-whatshelp",
+      options: {
+        // facebook: "xxx", // Facebook page ID
+        whatsapp: "+52 1 951 102 8474", // WhatsApp number
+        // email: "xxx@xxx.com", // Email
+        // call: "+52 1 951 102 8474", // Call phone number
+        company_logo_url:
+          "https://www.rutalab.com/static/logo_rutalab_final-afdf0e12313c122c09a65385cbc31980.png", // URL of company logo (png, jpg, gif)
+        greeting_message: "Hola...", // Text of greeting message
+        call_to_action: "Escríbenos", // Call to action
+        button_color: "#b02030", // Color of button
+        position: "right", // Position may be 'right' or 'left'
+        order: "facebook,whatsapp,call,email", // Order of buttons
+        ga: true, // Google Analytics enabled
+        branding: false, // Show branding string
+        mobile: true, // Mobile version enabled
+        desktop: true, // Desktop version enabled
+        greeting: true, // Greeting message enabled
+        shift_vertical: 0, // Vertical position, px
+        shift_horizontal: 0, // Horizontal position, px
+        domain: "https://www.rutalab.com/", // site domain
+        // key: "xxx", // pro-widget key
+      },
+    },
   ],
 };
